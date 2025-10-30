2a532a7acb6138ac45a9e06d56a020468a2a219c
ShwaTech <shwakesh13579@gmail.com>
2025-10-30 20:58:57 +0300

    Initial Project Structure Created

--- README.md
+++ README.md
@@ -2,6 +2,81 @@
 
 End-To-End Advanced RAG Project: build an Advanced RAG with LangChain, AstraDB, and utilizing advanced features (Memory, Evals, MMR); integrate with FastAPI APIs and UI (Streamlit/HTML/CSS); test using Pytest; and finally deploy to AWS ECS Fargate via CI/CD using GitHub Actions and Jenkins...
 
+## Project Structure
+
+```bash
+LLMOps-Advanced-RAG-Project/
+├── .github/
+│   └── workflows/
+│       └── task_defination.json
+│
+├── multi-doc-chat/
+│   ├── config/
+│   │   └── config.yaml
+│   │
+│   ├── exception/
+│   │   ├── __init__.py
+│   │   └── custom_exception.py
+│   │
+│   ├── logger/
+│   │   ├── __init__.py
+│   │   └── custom_logger.py
+│   │
+│   ├── model/
+│   │   ├── __init__.py
+│   │   └── models.py
+│   │
+│   ├── prompt/
+│   │   ├── __init__.py
+│   │   └── prompt_library.py
+│   │
+│   ├── src/
+│   │   ├── __init__.py
+│   │   ├── document_ingestion/
+│   │   │   ├── __init__.py
+│   │   │   └── data_ingestion.py
+│   │   │
+│   │   └── document_chat/
+│   │       ├── __init__.py
+│   │       └── retrieval.py
+│   │
+│   └── utils/
+│       ├── __init__.py
+│       ├── file_io.py
+│       ├── config_loader.py
+│       ├── model_loader.py
+│       └── document_ops.py
+│
+├── notebook/
+│   ├── 01-Experiments.ipynb
+│   ├── 02-RAG.ipynb
+│   └── 03-Evaluations.ipynb
+│
+├── test/
+│   ├── configure_test.py
+│   ├── integration/
+│   │   ├── test_chat_route.py
+│   │   └── test_upload_route.py
+│   └── unit/
+│       ├── test_data_ingestion.py
+│       └── test_retrieval.py
+│
+├── templates/
+│   └── index.html
+│
+├── static/
+│   └── style.css
+│
+├── main.py
+├── app.py
+├── test.py
+├── run_evaluations.py
+├── .dockerignore
+├── Dockerfile
+├── Jenkinsfile
+└── requirements.txt
+```
+
 ## Environment Setup
 
 ```bash
---
+++ template.py
@@ -0,0 +1,67 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "multi-doc-chat"
+
+list_of_files = [
+    ".github/workflows/task_defination.json",
+    f"{project_name}/config/config.yaml",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/exception/custom_exception.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/logger/custom_logger.py",
+    f"{project_name}/model/__init__.py",
+    f"{project_name}/model/models.py",
+    f"{project_name}/prompt/__init__.py",
+    f"{project_name}/prompt/prompt_library.py",
+    f"{project_name}/src/__init__.py",
+    f"{project_name}/src/document_ingestion/__init__.py",
+    f"{project_name}/src/document_ingestion/data_ingestion.py",
+    f"{project_name}/src/document_chat/__init__.py",
+    f"{project_name}/src/document_chat/retrieval.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/file_io.py",
+    f"{project_name}/utils/config_loader.py",
+    f"{project_name}/utils/model_loader.py",
+    f"{project_name}/utils/document_ops.py",
+    "notebook/01-Experiments.ipynb",
+    "notebook/02-RAG.ipynb",
+    "notebook/03-Evaluations.ipynb",
+    "test/configure_test.py",
+    "test/integration/test_chat_route.py",
+    "test/integration/test_upload_route.py",
+    "test/unit/test_data_ingestion.py",
+    "test/unit/test_retrieval.py",
+    "templates/index.html",
+    "static/style.css",
+    "main.py",
+    "app.py",
+    "test.py",
+    "run_evaluations.py",
+    ".dockerignore",
+    "Dockerfile",
+    "Jenkinsfile",
+    "requirements.txt",
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
